5c8c760dd5c6179a6c9d4ba1c69681d248c1f5b9
Ryan Springer <ryan.springer@datastax.com>
2020-08-04 14:36:50 -0500

    WIP - adding tolerations test

---
+++ tests/tolerations/tolerations_suite_test.go
@@ -0,0 +1,61 @@
+// Copyright DataStax, Inc.
+// Please see the included license file for details.
+
+package tolerations
+
+import (
+	"fmt"
+	"testing"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	ginkgo_util "github.com/datastax/cass-operator/mage/ginkgo"
+	"github.com/datastax/cass-operator/mage/kubectl"
+)
+
+var (
+	testName    = "Tolerations"
+	opNamespace = "test-tolerations"
+	dc1Name     = "dc1"
+	dc1Yaml     = "../testdata/tolerations-dc.yaml"
+	dc1Resource = fmt.Sprintf("CassandraDatacenter/%s", dc1Name)
+	ns          = ginkgo_util.NewWrapper(testName, opNamespace)
+)
+
+func TestLifecycle(t *testing.T) {
+	AfterSuite(func() {
+		logPath := fmt.Sprintf("%s/aftersuite", ns.LogDir)
+		err := kubectl.DumpAllLogs(logPath).ExecV()
+		if err != nil {
+			fmt.Printf("\n\tError during dumping logs: %s\n\n", err.Error())
+		}
+		fmt.Printf("\n\tPost-run logs dumped at: %s\n\n", logPath)
+		ns.Terminate()
+	})
+
+	RegisterFailHandler(Fail)
+	RunSpecs(t, testName)
+}
+
+var _ = Describe(testName, func() {
+	Context("when in a new cluster", func() {
+		Specify("the operator can build pods with tolerations", func() {
+
+			By("creating a namespace for the cass-operator")
+			err := kubectl.CreateNamespace(opNamespace).ExecV()
+			Expect(err).ToNot(HaveOccurred())
+
+			step := "setting up cass-operator resources via helm chart"
+			ns.HelmInstall("../../charts/cass-operator-chart")
+
+			ns.WaitForOperatorReady()
+
+			step = "creating first datacenter resource"
+			k := kubectl.ApplyFiles(dc1Yaml)
+			ns.ExecAndLog(step, k)
+
+			ns.WaitForDatacenterReady(dc1Name)
+		})
+	})
+})
