cdccbecdd6925b78c9a22c05289ee773baddf756
Ryan Springer <ryan.springer@datastax.com>
2020-08-11 10:30:56 -0500

    debugging

--- operator/pkg/reconciliation/handler.go
+++ operator/pkg/reconciliation/handler.go
@@ -35,9 +35,11 @@ func (rc *ReconciliationContext) calculateReconciliationActions() (reconcile.Res
 
 	rc.ReqLogger.Info("handler::calculateReconciliationActions")
 
-	if err := rc.checkNodeTaints(); err != nil {
-		return result.Error(err).Output()
-	}
+	/*
+		if err := rc.checkNodeTaints(); err != nil {
+			return result.Error(err).Output()
+		}
+	*/
 
 	// Check if the CassandraDatacenter was marked to be deleted
 	if result := rc.ProcessDeletion(); result.Completed() {
